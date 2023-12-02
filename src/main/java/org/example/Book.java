bcb7e3e0396e525a27604e10016fc7a43b055b0c
ivankulesh <ivankulesh0200@gmail.com>
2023-12-02 21:03:51 +0200

    create class and getters

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Book {
+    private String titleBook;
+    private String authorBook;
+    private String isbnBook;
+    private int yearBook;
+
+    public Book(String titleBook, String authorBook, String isbnBook, int yearBook)
+    {
+        this.titleBook = titleBook;
+        this.authorBook = authorBook;
+        this.isbnBook = isbnBook;
+        this.yearBook = yearBook;
+    }
+    //getters
+    public String getTitleBook(){
+        return titleBook;
+    }
+    public String getAuthorBookBook(){
+        return authorBook;
+    }
+    public String getIsbnBook(){
+        return isbnBook;
+    }
+    public String getYearBook(){
+        return yearBook;
+    }
+}
